2c7d4b38135b42f4ff1d8f411948754760b65677
Shiyue <tglchen17@gmail.com>
2026-02-06 21:42:09 +0800

    adjust the scope of annoucement

--- php-site/index.php
+++ php-site/index.php
@@ -1278,7 +1278,7 @@ function render_page(string $title, string $content, ?array $user = null, string
         echo "</div>";
     }
 
-    if ($layout !== 'app') {
+    if ($layout !== 'app' && $layout !== 'auth') {
         $announcements = get_active_announcements();
         if (should_show_announcement_modal($announcements)) {
             echo render_announcement_modal($announcements);
